619fed39316d5a517a3b8a6398e7602a08143640
Luis Nolazco <luisn@TTL002163.thetrainline.com>
2018-12-11 18:58:51 +0000

    add mignonne api to list view infinite screen

--- src/components/List/InfiniteListView.js
+++ src/components/List/InfiniteListView.js
@@ -20,15 +20,32 @@ export default class Application extends Component {
       isLoading: true,
       isLoadingMore: false,
       _data: null,
-      _dataAfter: '',
+      // _dataAfter: '',
+      page: 1,
     };
   }
 
   _fetchData(callback) {
-    const params =
-      this.state._dataAfter !== '' ? `&after=${this.state._dataAfter}` : '';
-    //Limits fetches to 15 so there's lesser items from the get go
-    fetch(`https://www.reddit.com/subreddits/popular/.json?limit=15${params}`)
+    // const params =
+    //   this.state._dataAfter !== '' ? `&after=${this.state._dataAfter}` : '';
+    // //Limits fetches to 15 so there's lesser items from the get go
+    // fetch(`https://www.reddit.com/subreddits/popular/.json?limit=15${params}`)
+    //   .then(response => response.json())
+    //   .then(callback)
+    //   .catch(error => {
+    //     console.error(error);
+    //   });
+
+    const page = this.state.page;
+    const token =
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvd3d3Lm1pZ25vbm5lLmNvbSIsImlhdCI6MTU0MjM1Njg4NSwiZXhwIjoxNTU3OTkxNjg1LCJpZCI6IjU5MzkxMCIsInRva2VuaWQiOiI1YmVlN2Y5NWVmZGE1In0.PTKPVCSahzE-yNBzhu-E4ApEN71FXv3QzS4Kz--HPrw';
+    const url = `https://www.mignonneapi.com/private/users?city_number=37995,37996,37997,37998&iso=BE&page=${page}&per_page=40&radius=50&region_number=1,2&type=global`;
+    fetch(url, {
+      method: 'GET',
+      headers: new Headers({
+        Authorization: `Bearer ${token}`,
+      }),
+    })
       .then(response => response.json())
       .then(callback)
       .catch(error => {
@@ -39,12 +56,13 @@ export default class Application extends Component {
   _fetchMore() {
     this.fetchData(responseJson => {
       const data = this.state._data.concat(responseJson.data.children);
-      this.setState({
+      this.setState(prevState => ({
         dataSource: this.state.dataSource.cloneWithRows(data),
         isLoadingMore: false,
         _data: data,
-        _dataAfter: responseJson.data.after,
-      });
+        // _dataAfter: responseJson.data.after,
+        page: prevState.page + 1,
+      }));
     });
   }
 
@@ -54,12 +72,13 @@ export default class Application extends Component {
       let ds = new ListView.DataSource({
         rowHasChanged: (r1, r2) => r1 !== r2,
       });
-      const data = responseJson.data.children;
+      const data = responseJson.data.users; // responseJson.data.children;
       this.setState({
         dataSource: ds.cloneWithRows(data),
         isLoading: false,
         _data: data,
-        _dataAfter: responseJson.data.after,
+        page: 1,
+        // _dataAfter: responseJson.data.after,
       });
     });
   }
